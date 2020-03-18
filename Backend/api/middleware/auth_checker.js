1d10713e6c9f78c52ac3130e5336c7a94c896b6b
pasindu prabath hettiarachchi <pasindu.p.hettiarachchi@gmail.com>
2020-03-18 21:31:05 +0530

    added auth_checker.js middleware

--- Backend/api/middleware/auth_checker.js
+++ Backend/api/middleware/auth_checker.js
@@ -8,7 +8,7 @@ module.exports = (req,res,next)=> {
     }catch (error) {
 
         return res.status(401).json({
-            message: 'Auth fail'
+            message: 'Authentication failed'
         })
 
     }
