6f7341365f26cac775d91f24835bbc8ccf96ac66
pasindu prabath hettiarachchi <pasindu.p.hettiarachchi@gmail.com>
2020-03-18 21:44:41 +0530

    added image.js to the models

---
+++ Backend/api/models/image.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose")
+
+const imageSchema = mongoose.Schema({
+    _id : mongoose.Schema.Types.ObjectId,
+    image : {type:String, required:true}
+});
+module.exports = mongoose.model('image',imageSchema);
