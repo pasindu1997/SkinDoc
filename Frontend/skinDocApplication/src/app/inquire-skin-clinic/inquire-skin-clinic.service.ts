8fc240fa2a870ea54927099a0d1264bbb5508818
RakeebNoordeen <noordeen.2017444@iit.ac.lk>
2020-04-03 20:17:18 +0530

    I have added Inquire Skin Clinic

--- Frontend/skinDocApplication/src/app/inquire-skin-clinic/inquire-skin-clinic.service.ts
+++ Frontend/skinDocApplication/src/app/inquire-skin-clinic/inquire-skin-clinic.service.ts
@@ -11,7 +11,7 @@ export class InquireSkinClinicService {
         'Durdan Hospital',
         '"Durdans Hospital is a multi-speciality private hospital that treats patients visiting from around the world, in Colombo, Sri Lanka."',
         'https://www.durdans.com/wp-content/uploads/2017/09/21751864_1646442848731003_4694675338382260969_n-300x200.jpg',
-        'Address : 3 Alfred Pl, Colombo 00300',
+        'Address : 3 Alfred Pl,Colombo 00300',
         'Phone : 0112 140 000'
     ),
     new skinClinic(
@@ -19,7 +19,7 @@ export class InquireSkinClinicService {
         'Nawaloka Hospital',
         '"The entry of Nawaloka Hospitals into the state dominated healthcare sector in 1985, saw the private health care system take root in Sri Lanka."',
         'http://www.nawaloka.com/catalog/view/theme/default/image/why-us/nawaloka-hospital-slide-02.jpg',
-        'Address : 23, Deshamanya H. K Dharmadasa Mawatha, Colombo 2, Sri Lanka',
+        'Address : 23, Deshamanya H.K Dharmadasa Mawatha,Colombo2,Sri Lanka',
         'Phone : 0115 577 111'
     ),
     new skinClinic(
@@ -27,7 +27,7 @@ export class InquireSkinClinicService {
         'Skin Clinic Colombo',
         '"Discover the finest skin care clinic in Sri Lanka. Dermatology experts from our clinic are here to help you to gain and maintain a beautiful skin"',
         'https://media.timeout.com/images/103758149/1024/576/image.jpg',
-        'Address : 32 Lauries Rd, Colombo 00400',
+        'Address : 32 Lauries Rd,Colombo 00400',
         'Phone : 0112 588 679'
     )
   ];
