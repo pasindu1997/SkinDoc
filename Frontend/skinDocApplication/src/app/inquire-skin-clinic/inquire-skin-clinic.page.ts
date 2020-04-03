90fa375fc48c9e12259a3af89f1384fd50fbc6de
RakeebNoordeen <noordeen.2017444@iit.ac.lk>
2020-04-03 20:05:34 +0530

    I have added Inquire Skin Clinic Part

--- Frontend/skinDocApplication/src/app/inquire-skin-clinic/inquire-skin-clinic.page.ts
+++ Frontend/skinDocApplication/src/app/inquire-skin-clinic/inquire-skin-clinic.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {InquireSkinClinicService} from './inquire-skin-clinic.service';
+import {skinClinic} from './skinClinic.model';
 
 @Component({
   selector: 'app-inquire-skin-clinic',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inquire-skin-clinic.page.scss'],
 })
 export class InquireSkinClinicPage implements OnInit {
+  loadedPlaces: skinClinic[];
 
-  constructor() { }
+  constructor(private placesService: InquireSkinClinicService) { }
 
   ngOnInit() {
+    this.loadedPlaces = this.placesService.clinics;
   }
 
 }
