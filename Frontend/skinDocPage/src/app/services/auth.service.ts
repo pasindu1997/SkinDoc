4bafc6011827f4d23ebd049ac27662df9b990879
Gayal1998 <gayal.2018166@iit.ac.lk>
2020-04-12 11:03:44 +0530

    get jwt authentication an

--- Frontend/skinDocPage/src/app/services/auth.service.ts
+++ Frontend/skinDocPage/src/app/services/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
+
+const helper=new JwtHelperService();
+const TOKEN_KEY='jwt-token';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private storage: Storage,private http:HttpClientModule,
+    private plt:Platform , private router:Router) { 
+
+    }
 }
