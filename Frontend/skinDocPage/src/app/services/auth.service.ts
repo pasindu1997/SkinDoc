5a753215143888b509efa5fc4042ad31d560679a
Gayal1998 <gayal.2018166@iit.ac.lk>
2020-04-12 11:30:41 +0530

    use rxjs switchMap operator and  update auth.service.ts file

--- Frontend/skinDocPage/src/app/services/auth.service.ts
+++ Frontend/skinDocPage/src/app/services/auth.service.ts
@@ -3,7 +3,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
 import{JwtHelperService} from '@auth0/angular-jwt'; 
-
+import { Observable, BehaviorSubject, from } from 'rxjs';
+import{take,map,switchMap} from 'rxjs/operators';
 
 const helper=new JwtHelperService();
 const TOKEN_KEY='jwt-token';
@@ -12,9 +13,24 @@ const TOKEN_KEY='jwt-token';
   providedIn: 'root'
 })
 export class AuthService {
+  public user : Observable<any>;
+  private userData= new BehaviorSubject(null);
 
   constructor(private storage: Storage,private http:HttpClientModule,
     private plt:Platform , private router:Router) { 
 
+    }
+
+    loadStoredToken(){
+      let PlatformObs=from(this.plt.ready());
+      this.user=PlatformObs.pipe(
+          switchMap(() => {
+           return from (this.storage.get(TOKEN_KEY))
+          }),
+          map(token =>{
+            console.log('token from storage:' ,token)
+          })
+
+      );
     }
 }
