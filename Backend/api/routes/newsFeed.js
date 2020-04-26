6c6a520b9b2b5e53c3dd2b0538b028a84ed7992d
Kawshika-codeMasterXeroa <kawshith123senu@gmail.com>
2020-04-26 05:41:25 +0530

    Finalizing the backend part of Kawshika Siyasinghe.

--- Backend/api/routes/newsFeed.js
+++ Backend/api/routes/newsFeed.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 // const mongoose = require('mongoose');
 
-router.post('/rateclinic', () => {
-
+router.get('/newsUpdate', () => {
+//this will call an API to skin cancer news service (This will be implemented only if valid API service found)
 });
 
 module.exports = router;
